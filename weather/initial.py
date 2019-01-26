716b29b7c3a5d50331f30c35aee46c96d9d608bf
Max <mpage-sowik@outlook.com>
2019-01-26 17:06:43 -0500

    merging

--- weather/initial.py
+++ weather/initial.py
@@ -12,19 +12,11 @@ def getData():
 
     for row in query_job:  # API request - fetches results
         # Row values can be accessed by field name or index
-<<<<<<< HEAD
         # assert row["state"]
 
    #     return row[7]
         print(row[9])
         #print("\n")
-=======
-        # assert row[0] == row.name == row["state"]
-
-        # return row[7]
-        print(row[7])
-        print("\n")
->>>>>>> 4056bebf7f415a9997203148c194c6a9cb006317
         #noaa_tsunami
         #noaa_significant_earthquakes
         #noaa_spc
