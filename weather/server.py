5721ff1e81accd218d735ebdc55c00b6eb4db912
Max <mpage-sowik@outlook.com>
2019-01-26 18:06:33 -0500

    front end

--- weather/server.py
+++ weather/server.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify, render_template
 from initial import getData
+from plotting import build_plot
+#from bokeh import 
 import json
 app = Flask(__name__)
 
+@app.route('/plot')
+def render_plot():
+    plot_snippet = build_plot()
+    
+    return render_template('plots.html', snippet=plot_snippet)
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html")
 
 @app.route("/hello")
 def h():
