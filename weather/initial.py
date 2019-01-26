d2065480ea29fe2ee39d791876012c99a6c8cb2e
Jesse Silber <jessesilber@gmail.com>
2019-01-26 14:52:48 -0500

    my changes

--- weather/initial.py
+++ weather/initial.py
@@ -1,9 +1,7 @@
 from google.cloud import bigquery
 client = bigquery.Client()
 query = (
-    "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
-    'WHERE state = "TX" '
-    "LIMIT 100"
+    "SELECT * FROM `bigquery-public-data.noaa_tsunami.historical_runups` LIMIT 10"
 )
 query_job = client.query(
     query,
@@ -11,9 +9,12 @@ query_job = client.query(
     location="US",
 )  # API request - starts the query
 
+
 for row in query_job:  # API request - fetches results
     # Row values can be accessed by field name or index
-    assert row[0] == row.name == row["name"]
-    print(row)
+    assert row[0] 
+    # == row.name == row["name"]
+    print(row[7])
+    print("\n")
 
 #from google.cloud import storage
