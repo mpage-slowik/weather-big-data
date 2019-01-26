c1579207520f5414046578e172a70e05890938f5
Max <mpage-sowik@outlook.com>
2019-01-26 13:30:34 -0500

    initial commit

---
+++ weather/initial.py
@@ -0,0 +1,19 @@
+from google.cloud import bigquery
+client = bigquery.Client()
+query = (
+    "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
+    'WHERE state = "TX" '
+    "LIMIT 100"
+)
+query_job = client.query(
+    query,
+    # Location must match that of the dataset(s) referenced in the query.
+    location="US",
+)  # API request - starts the query
+
+for row in query_job:  # API request - fetches results
+    # Row values can be accessed by field name or index
+    assert row[0] == row.name == row["name"]
+    print(row)
+
+#from google.cloud import storage
