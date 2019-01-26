2e37813f38bb7d584995030f6b4a12987056e874
Max <mpage-sowik@outlook.com>
2019-01-26 15:38:34 -0500

    stash

--- weather/initial.py
+++ weather/initial.py
@@ -1,20 +1,21 @@
 from google.cloud import bigquery
-client = bigquery.Client()
-query = (
-    "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
-    'WHERE state = "TX" '
-    "LIMIT 100"
-)
-query_job = client.query(
-    query,
-    # Location must match that of the dataset(s) referenced in the query.
-    location="US",
-)  # API request - starts the query
+def getData():
+    client = bigquery.Client()
+    query = (
+        "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
+        'WHERE state = "TX" '
+        "LIMIT 100"
+    )
+    query_job = client.query(
+        query,
+        # Location must match that of the dataset(s) referenced in the query.
+        location="US",
+    )  # API request - starts the query
 
-for row in query_job:  # API request - fetches results
-    # Row values can be accessed by field name or index
-    assert row[0] == row.name == row["name"]
-    print(row)
-#noaa_tsunami
-#noaa_significant_earthquakes
-#noaa_spc
+    for row in query_job:  # API request - fetches results
+        # Row values can be accessed by field name or index
+        assert row[0] == row.name == row["name"]
+        print(row)
+    #noaa_tsunami
+    #noaa_significant_earthquakes
+    #noaa_spc
--- weather/server.py
+++ weather/server.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from initial import getData
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
-    
+
+@app.route("/hello")
+def h():
+    return "HEY IM HERE"
+    #return getData()
+
 if __name__ == '__main__':
     app.run(debug=True)
