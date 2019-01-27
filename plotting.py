a9a703caa118adb395ad117fbbd13bdecdab06f8
Max <mpage-sowik@outlook.com>
2019-01-27 01:51:45 -0500

    threading

--- plotting.py
+++ plotting.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import mplleaflet
+import threading, time
 from Model import earthquake,tsunami,weather
 import pandas as pd
 from random import randint
@@ -7,32 +8,45 @@ from string import Template
 class Threading(object):
     def __init__(self, interval=1):
         self.interval = interval
-        thread = threading.Thread(target=self.plotPoints, args=("",))
+        thread = threading.Thread(target=self.plotPoints, args=("earthquakes",earthquake.getData(),'go'))
         thread.daemon = True
         thread.start()
-    def plotPoints(map_name,lat_long,cl):
-        fig, ax = plt.subplots()
-        x = [lat_long['lng']]
-        y = [lat_long['lat']]
-        ax.plot(x, y, cl)
-        maps="templates/"+map_name+".html"
-        f=open(maps,"w")
-        f.write(mplleaflet.fig_to_html(fig=fig))
-        f.close()
-    def plotWeather(map_name,hail_la_ln,wind_la_ln,tornado_la_ln):
-        fig, ax = plt.subplots()
-        x = [hail_la_ln['lng']]
-        y = [hail_la_ln['lat']]
-        ax.plot(x, y, 'b*')
-        x = [wind_la_ln['lng']]
-        y = [wind_la_ln['lat']]
-        ax.plot(x, y, 'b>')
-        x = [tornado_la_ln['lng']]
-        y = [tornado_la_ln['lat']]
-        ax.plot(x, y, 'b^')
-        maps="templates/"+map_name+".html"
-        f=open(maps,"w")
-        f.write(mplleaflet.fig_to_html(fig=fig))
-        f.close()
+        thread = threading.Thread(target=self.plotPoints, args=("tsunami",tsunami.getDataTsunami(),'bo'))
+        thread.daemon = True
+        thread.start()
+        thread = threading.Thread(target=self.plotWeather, args=("weather","weather",weather.getHailReports(),weather.getWindReports(),weather.getTornadoReports()))
+        thread.daemon = True
+        thread.start()
+
+    def plotPoints(self,map_name,lat_long,cl):
+        while(True):
+            fig, ax = plt.subplots()
+            x = [lat_long['lng']]
+            y = [lat_long['lat']]
+            ax.plot(x, y, cl)
+            maps="templates/"+map_name+".html"
+            f=open(maps,"w")
+            f.write(mplleaflet.fig_to_html(fig=fig))
+            f.close()
+            time.sleep(self.interval)
+    def plotWeather(self,map_name,hail_la_ln,wind_la_ln,tornado_la_ln):
+        while(True):
+            fig, ax = plt.subplots()
+            x = [hail_la_ln['lng']]
+            y = [hail_la_ln['lat']]
+            ax.plot(x, y, 'b*')
+            x = [wind_la_ln['lng']]
+            y = [wind_la_ln['lat']]
+            ax.plot(x, y, 'b>')
+            x = [tornado_la_ln['lng']]
+            y = [tornado_la_ln['lat']]
+            ax.plot(x, y, 'b^')
+            maps="templates/"+map_name+".html"
+            f=open(maps,"w")
+            f.write(mplleaflet.fig_to_html(fig=fig))
+            f.close()
+            time.sleep(self.interval)
+#start threading
+threads = Threading(600)
 # if __name__ == '__main__':
 #     plotPoints("templates/default.html")
--- server.py
+++ server.py
@@ -1,6 +1,4 @@
 from flask import Flask, jsonify, render_template
-from weather.plotting import plotPoints,plotWeather
-from Model import earthquake,tsunami,weather
 import json
 app = Flask(__name__)
 
@@ -9,22 +7,21 @@ app = Flask(__name__)
 def hello():
     #print(earthquake.getData())
     #fig1 = plotPoints()
-
     return render_template("index.html")
 
 @app.route("/Tsunami")
 def hur():
-    plotPoints("tsunami",tsunami.getDataTsunami(),'bo')
+   # plotPoints("tsunami",tsunami.getDataTsunami(),'bo')
     return render_template("tsunami.html")
 
 @app.route("/Earthquakes")
 def ert():
-    plotPoints("earthquakes",earthquake.getData(),'go')
+    #plotPoints("earthquakes",earthquake.getData(),'go')
     return render_template("earthquakes.html")
 
 @app.route("/Weather")
 def wea():
-    plotWeather("weather",weather.getHailReports(),weather.getWindReports(),weather.getTornadoReports())
+    #plotWeather("weather",weather.getHailReports(),weather.getWindReports(),weather.getTornadoReports())
     return render_template("weather.html")
 
 if __name__ == '__main__':
